be6c8fbdd7d81f6d7712f9b5a5aac47f8caf58a5
Igor <igor_motyka@mail.com>
2022-01-08 21:38:59 +0100

    Added printing of chunk data

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,62 @@
-use std::fs;
+use std::{env, fs};
 
 use png::ChunkIterator;
+
+use crate::png::ChunkTypes;
 
 mod png;
 
 fn main() {
-    let file_bytes = fs::read("./image.png").expect("Cannot read file");
+    let args: Vec<String> = env::args().collect();
+
+    let file_path = &args[1];
+
+    for arg in &args {
+        match arg.as_str() {
+            "-s" => {}
+            "-h" => {}
+            _ => {}
+        }
+    }
+
+    let file_bytes = fs::read(file_path).expect("Cannot read file");
 
     let chunk_iter = ChunkIterator::new(&file_bytes);
 
-    for chunk in chunk_iter {}
+    for chunk in chunk_iter {
+        match chunk {
+            ChunkTypes::IHDR(data) => {
+                println! {"IHDR:"}
+                println!(
+                    "\tSize: {}x{}px",
+                    u32::from_be_bytes(data.width),
+                    u32::from_be_bytes(data.height)
+                );
+                println!("\tBit depth: {}", data.bit_depth);
+                println!("\tColor type: {}", data.color_type);
+                println!("\tCompression method: {}", data.compression_method);
+                println!("\tFilter method: {}", data.filter_method);
+                println!("\tInterlace method: {}", data.interlace_method);
+            }
+            ChunkTypes::IDAT(data) => {
+                println!("IDAT:");
+            }
+            ChunkTypes::PLTE(data) => {
+                println!("PLTE:");
+            }
+            ChunkTypes::IEND => {
+                println!("IEND:");
+            }
+            ChunkTypes::Text(text) => {
+                println!("Text:");
+                println!("\t{}", text)
+            }
+            ChunkTypes::Unknown(chunk_type_str, chunk_data_size, bytes) => {
+                println!("Unknown chunk:");
+                println!("\t Type: {}", chunk_type_str);
+                println!("\t Data size: {}", chunk_data_size);
+                println!("\t Bytes: {:?}", bytes);
+            }
+        }
+    }
 }
--- src/png.rs
+++ src/png.rs
@@ -11,8 +11,9 @@ pub enum ChunkTypes {
     IHDR(IHDR),
     IDAT(IDAT),
     PLTE(PLTE),
-    IEND(IEND),
-    Unknown,
+    IEND,
+    Text(String),
+    Unknown(String, usize, Vec<u8>),
 }
 
 #[derive(Debug)]
@@ -40,8 +41,6 @@ pub struct IDAT {
     pub image_data: [u8; 32],
 }
 
-pub struct IEND {}
-
 pub struct ChunkIterator<'a> {
     i: usize,
     bytes: &'a Vec<u8>,
@@ -58,9 +57,6 @@ impl<'a> ChunkIterator<'a> {
         let width: [u8; 4] = self.bytes[start..start + 4].try_into().unwrap();
         let height: [u8; 4] = self.bytes[start + 4..start + 8].try_into().unwrap();
 
-        println!("Width: {:?} = {}px", width, u32::from_be_bytes(width));
-        println!("Height: {:?} = {}px", height, u32::from_be_bytes(height));
-
         let chunk = IHDR {
             width,
             height,
@@ -70,13 +66,8 @@ impl<'a> ChunkIterator<'a> {
             filter_method: self.bytes[start + 11],
             interlace_method: self.bytes[start + 12],
         };
-        println!("Bit depth: {}", chunk.bit_depth);
-        println!("Color type: {}", chunk.color_type);
-        println!("Compression method: {}", chunk.compression_method);
-        println!("Filter method: {}", chunk.filter_method);
-        println!("Interlace method: {}", chunk.interlace_method);
-        //TODO: Add CRC parsing
 
+        //TODO: Add CRC parsing
         ChunkTypes::IHDR(chunk)
     }
 
@@ -92,10 +83,12 @@ impl<'a> ChunkIterator<'a> {
         });
     }
 
-    pub(crate) fn parse_iend(&self) -> ChunkTypes {
-        return ChunkTypes::IDAT(IDAT {
-            image_data: [0; 32],
-        });
+    pub(crate) fn parse_text(&self, data_length: usize) -> ChunkTypes {
+        let start = self.i + 8; //Skipping chunk length and type
+        let text = std::str::from_utf8(&self.bytes[start..start + data_length])
+            .expect("Failed to read text from chunk");
+
+        ChunkTypes::Text(String::from(text))
     }
 }
 
@@ -103,32 +96,33 @@ impl<'a> Iterator for ChunkIterator<'a> {
     type Item = ChunkTypes;
 
     fn next(&mut self) -> Option<Self::Item> {
-        println!("============");
         if self.i + 8 > self.bytes.len() {
             return None;
         }
-        let chunk_length: [u8; 4] = self.bytes[self.i..self.i + 4].try_into().unwrap();
-        let chunk_type: [u8; 4] = self.bytes[self.i + 4..self.i + 8].try_into().unwrap();
+        let chunk_length: [u8; 4] = self.bytes[self.i..self.i + 4]
+            .try_into()
+            .expect("Failed to read chunk data length");
+        let chunk_type: [u8; 4] = self.bytes[self.i + 4..self.i + 8]
+            .try_into()
+            .expect("Failed to read chunk type");
         let chunk_length_in_bytes = u32::from_be_bytes(chunk_length);
-        println!(
-            "Chunk data length: {:?} = {}b",
-            chunk_length, chunk_length_in_bytes
-        );
 
         let chunk_type_str = std::str::from_utf8(&chunk_type).expect("Failed to read chunk type");
-        println!("Chunk type bytes: {:?} = {}", chunk_type, chunk_type_str);
 
         //Chunk length + chunk type + data length + CRC
         let chunk_total_length = (4 + 4 + chunk_length_in_bytes + 4) as usize;
 
-        println!("Total chunk size: {}b", chunk_total_length);
-
-        let chunk_type = match chunk_type_str {
-            "IHDR" => self.parse_ihdr(),
-            "PLTE" => self.parse_plte(),
-            "IDAT" => self.parse_Idat(),
-            "IEND" => self.parse_iend(),
-            _ => ChunkTypes::Unknown,
+        let chunk_type = match chunk_type_str.to_lowercase().as_str() {
+            "ihdr" => self.parse_ihdr(),
+            "plte" => self.parse_plte(),
+            "idat" => self.parse_Idat(),
+            "iend" => ChunkTypes::IEND,
+            "text" => self.parse_text(chunk_length_in_bytes as usize),
+            _ => ChunkTypes::Unknown(
+                String::from(chunk_type_str),
+                chunk_length_in_bytes as usize,
+                self.bytes[self.i..self.i + chunk_total_length].to_vec(),
+            ),
         };
 
         self.i += chunk_total_length;
